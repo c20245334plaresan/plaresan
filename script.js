9558ea91d8b21950fa8c7448cf8ff46327f39c52
plaresan <c20245334plaresan@csav.edu.ph>
2026-09-11 12:27:00 +0800

    addEventListener

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-console.log("Hello, world!");
+console.log("Hello mundo")
+const Myname = "John Arvin Plaresan";
+console.log("name: " + Myname);
+let age = 20;
+let number = "09216600020";
+console.log("Age: " + age)
+console.log("Number: " + number);
 
-const myName = "John Arvin Plaresan";
-let age = 21;
-const number = "09216600020";
-const address = "BRGY XI, GAWAHON,VICTORIAS CITY, NEGROS OCCIDENTAL";
+const address = "BRGY, XI,GAWHON,VICTORIAS CITY,NECROS OCCIDENTAL";
+console.log("Address: " + address);
 
-console.log(`Name ${myName}`);
-console.log(`Age ${age}`);
-console.log(`Number ${number}`);
-console.log(`Address ${address}`)
+function greet(name) {
 
-const heading = document.querySelector("h1");
- console.log (contactHeading);
+    return `good morning, ${name}`;
 
 
-const projectHeading = document.querySelector("h1");
- console.log (projectHeading);
+}
+console.log(greet("rene"))
 
- // text content
-heading.textContent = "My Portfolio";
-contactHeading.textContent = "lets connect";
+const heading = document.querySelector("h1")
+console.log(heading);
+
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+
+const servicesHeading = document.querySelector("#services h2");
+console.log(servicesHeading);
+
+const projectHeading = document.querySelector("#projects h2");
+console.log(projectHeading);
+
+
+
+
+//Text Context
+heading.textContent = "My portfolio";
+contactHeading.textContent = "Let's Connect!";
 projectHeading.textContent = "My Projects";
 
-// style
 heading.style.color = "purple";
-heading.style. backgroundColor = "yellow";
+heading.style.backgroundColor = "yellow";
 contactHeading.style.color = "purple";
-serviceHeading.style.color = "purple";
-const projectheading = document.querySelector("#projects");
-projectHeading . style . color ="purple";
+projectHeading.style.color = "purple";
+servicesHeading.style.color = "purple";
+
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+});
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+
+    }
+});
+
