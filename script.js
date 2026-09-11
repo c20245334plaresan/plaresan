fd2b23747aba96ccd4b4719f49d780244231fdae
plaresan <c20245334plaresan@csav.edu.ph>
2026-09-11 10:45:04 +0800

    functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello, world!");
-
 const myName = "John Arvin Plaresan";
 let age = 21;
 const number = "09216600020";
@@ -19,5 +17,36 @@ function greet(name) {
 
 console.log (greet (`John Arvin`));
 
+function add (a,b) {
+    return a + b;
+}
+function mdas (num1,num2) {
+    let m = num1 * num2;
+    let d = num1 / num2;
+    let a = num1 + num2;
+    let s = num1 - num2;
+    return (`values: ${num1} and ${num2}, product: ${m}, quotient: ${d}, sum: ${a}, difference: ${s}`);
+    
+    }
+   
+   console.log (mdas(5,3));
+
+
+
+
+function add (a,b) {
+    return a + b;
+}
+function mdas (a,b) {
+    let sum = a + b;
+    let difference = a - b;
+    let product = a * b;
+    let quotient = a / b;
+
+    return (`values: ${num1} and ${num2}, product: ${m}, quotient: ${d}, sum: ${a}, difference: ${s}`);
+    
+    
+} 
+console.log(mdas(5,3));
 
 
