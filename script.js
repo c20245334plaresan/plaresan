7ca7d63b66301d1f9cd71dd577a3cbb54682f2a8
plaresan <c20245334plaresan@csav.edu.ph>
2026-09-11 10:34:55 +0800

    Variable (let & const)

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello, world!");
+
+const myName = "John Arvin Plaresan";
+let age = 21;
+const number = "09216600020";
+const address = "BRGY XI GAWAHON,VICTORIAS CITY,";
+
+console.log(`Name ${myName}`);
+console.log(`Age ${age}`);
+console.log(`Number ${number}`);
+console.log(`Address ${address}`)
+
+// Functions
+
+function greet(name) {
+    return `Good morning, ${name}!`;
+
+}
+
+console.log (greet (`John Arvin`));
+
+
+
