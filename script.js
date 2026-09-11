bf78df35b43bbea2e60033f128ed9d8db05e39e8
plaresan <c20245334plaresan@csav.edu.ph>
2026-09-11 11:21:36 +0800

    querySelector

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello, world!");
+
+const myName = "John Arvin Plaresan";
+let age = 21;
+const number = "09216600020";
+const address = "BRGY XI, GAWAHON,VICTORIAS CITY, NEGROS OCCIDENTAL";
+
+console.log(`Name ${myName}`);
+console.log(`Age ${age}`);
+console.log(`Number ${number}`);
+console.log(`Address ${address}`)
+
+const heading = document.querySelector("h1");
+ console.log (contactHeading);
+
+
+const projectHeading = document.querySelector("h1");
+ console.log (projectHeading);
